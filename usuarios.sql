ed500bd0d343dbbd6f061f3589c736569373bda6
lsilvao-senati <lsilvao.senati@gmail.com>
2025-08-12 13:31:56 -0500

    Delete usuarios.sql

--- usuarios.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre_usuario` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `rol` enum('administrador','doctor','recepcionista') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
